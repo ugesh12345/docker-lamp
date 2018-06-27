7e044a7d9b1751cb04835944c595eb1c3cb3555c
root <root@ip-172-31-80-218.ec2.internal>
2018-06-27 14:33:13 +0000

    phpcommit

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <?php
 echo "<h2>PHP is Fun!</h2>";
 echo "Hello world hai!<br>";
-echo "I'm about to learn PHP!<br>";
+echo "I'm about to learn PHP!and docker<br>";
 echo "This ", "string ", "was ", "made ", "with multiple parameters.";
 ?> 
 
