9fd95242a15b3e5705f510afed4837cd205117d0
root <root@ip-172-31-80-218.ec2.internal>
2018-06-27 07:59:39 +0000

    phpcommit2

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 <?php
 echo "<h2>PHP is Fun!</h2>";
-echo "Hello world!<br>";
+echo "Hello world hai!<br>";
 echo "I'm about to learn PHP!<br>";
 echo "This ", "string ", "was ", "made ", "with multiple parameters.";
 ?> 
